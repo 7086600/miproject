e9b91b87a2eba39ad2549f7f828aa925e8b75112
AK <67439739+7086600@users.noreply.github.com>
2024-04-25 00:07:12 +0300

    create medical recors db

--- medicalRecordsDB.py
+++ medicalRecordsDB.py
@@ -5,6 +5,7 @@ medicalCosts = {}
 medicalCosts["Marina"] = 6607.0
 medicalCosts.update({"Vinay": 3225.0})
 medicalCosts.update({"Connie": 8886.0, "Isaac": 16444.0, "Valentina": 6420.0})
+# update Vinay
 medicalCosts["Vinay"] = 3325.0
 print(medicalCosts)
 
@@ -25,11 +26,27 @@ ages = [27, 24, 43, 35, 52]
 zippedAges = zip(names, ages)
 # make dict by using a list comprehension
 namesToAges = {key: value for key, value in zippedAges} # or simple: namesToAges = dict(zippedAges)
-print(namesToAges)
+#print(namesToAges)
 
 # extract records
 
 marina_age = namesToAges.get("Marina", None)
 print("Marina's age is {}".format(marina_age))
 
+# create medical records db
 
+medical_records = {}
+medical_records["Marina"] = {"Age": 27, 
+                             "Sex": "Female", 
+                             "BMI": 31.1, 
+                             "Children": 2, 
+                             "Smoker": "Non-smoker", 
+                             "Insurance_cost": 6607.0}
+
+medical_records.update({"Vinay": {"Age": 24, "Sex": "Male", "BMI": 26.9, "Children": 0, "Smoker": "Non-smoker", "Insurance_cost": 3225.0},
+                        "Connie": {"Age": 43, "Sex": "Female", "BMI": 25.3, "Children": 3, "Smoker": "Non-smoker", "Insurance_cost": 8886.0},
+                        "Isaac": {"Age": 35, "Sex": "Male", "BMI": 20.6, "Children": 4, "Smoker": "Smoker", "Insurance_cost": 16444.0},
+                        "Valentina": {"Age": 52, "Sex": "Female", "BMI": 18.7, "Children": 1, "Smoker": "Non-smoker", "Insurance_cost": 6420.0}})
+
+# print(medical_records)
+print("Connie's insurance cost is {} dollars.".format(medical_records["Connie"]["Insurance_cost"]))
